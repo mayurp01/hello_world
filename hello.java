a562a4396d84fa15bf0a24e20408d1339f40bf30
Garrett Dancik <garrett.dancik@gmail.com>
2021-10-26 21:43:42 -0400

    first commit

---
+++ hello.java
@@ -0,0 +1,17 @@
+// a simple program to practice common Github commands 
+
+public class hello {
+
+    public static void main(String [] args) {
+
+       // initialize variables
+       String firstName = "Alex";
+	
+ 	   // output welcome message
+       System.out.println("Change this to a welcome message using " + 
+                          "the variable firstName");
+	 
+
+    }
+}
+
