024039863e4adc18868b255df5333d27a9664c1f
mayurp01 <mayur.p191997@gmail.com>
2022-11-10 15:42:06 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,11 +5,11 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = "Mayur";
 	
  	   // output welcome message
-       System.out.println("Change this to a welcome message using " + 
-                          "the variable firstName");
+       System.out.println("Hello My Name is" + 
+                          "Mayur");
 	 
 
     }
